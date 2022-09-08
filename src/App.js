86f93ac794f960c2efa03e961c0fb88134a7f007
Max Wallace <max.wallace9@yahoo.com>
2022-09-08 14:08:53 -0500

    separating out the category cards

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
+import CategoryItem from './components/category-item/category-item.component';
 import './categories.styles.scss'
 
+// This is the base for the home page of the website
 const App = () => {
 
+  // This array allows us to loop through the 
+  // different info for each card with map()
   const categories = [
     {
       id: 1,
-      title: 'Tech'
+      title: 'Tech',
+      imageUrl: 'https://i.ibb.co/TRdfRZS/tech.jpg'
     },
     {
       id: 2,
-      title: 'Outdoors'
+      title: 'Outdoors',
+      imageUrl: 'https://i.ibb.co/kyW753T/outdoors.jpg'
     },
     {
       id: 3,
-      title: 'Compact'
+      title: 'Compact',
+      imageUrl: 'https://i.ibb.co/9G0yD9F/compact.jpg'
     },
     {
       id: 4,
-      title: 'Overnight'
+      title: 'Overnight',
+      imageUrl: 'https://i.ibb.co/qs1N4vK/overnight.jpg'
     },
     {
       id: 5,
-      title: 'Everyday'
+      title: 'Everyday',
+      imageUrl: 'https://i.ibb.co/qMXKvxQ/everyday.jpg'
     }
   ];
 
+  // Using map() we are able to loop through the 
+  // array above to build the different cards while
+  // only having to type the syntax once
   return (
     <div className="categories-container">
-      {categories.map(({title, id}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key = {category.id} category={category} />
       ))};
     </div>
   );
