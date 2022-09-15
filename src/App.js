7be67d4b62e53dedc231b0072fb92ea722186c3b
Max Wallace <max.wallace9@yahoo.com>
2022-09-15 11:50:53 -0500

    Setting up routing, and getting the home page setup accordingly with the route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import CategoryMenu from './components/categories-menu/categories-menu.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component'
 
 
 // This is the base for the home page of the website
 const App = () => {
 
-  // This array allows us to loop through the 
-  // different info for each card with map()
-  const categories = [
-    {
-      id: 1,
-      title: 'Tech',
-      imageUrl: 'https://i.ibb.co/TRdfRZS/tech.jpg'
-    },
-    {
-      id: 2,
-      title: 'Outdoors',
-      imageUrl: 'https://i.ibb.co/kyW753T/outdoors.jpg'
-    },
-    {
-      id: 3,
-      title: 'Compact',
-      imageUrl: 'https://i.ibb.co/9G0yD9F/compact.jpg'
-    },
-    {
-      id: 4,
-      title: 'Overnight',
-      imageUrl: 'https://i.ibb.co/qs1N4vK/overnight.jpg'
-    },
-    {
-      id: 5,
-      title: 'Everyday',
-      imageUrl: 'https://i.ibb.co/qMXKvxQ/everyday.jpg'
-    }
-  ];
-
-
   return (
-    <CategoryMenu categories={ categories }/>
+    <Routes>
+      <Route path='/home' element = {<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,43 @@
+import CategoryMenu from '../../components/categories-menu/categories-menu.component';
+
+
+// This is the base for the home page of the website
+const Home = () => {
+
+  // This array allows us to loop through the 
+  // different info for each card with map()
+  const categories = [
+    {
+      id: 1,
+      title: 'Tech',
+      imageUrl: 'https://i.ibb.co/TRdfRZS/tech.jpg'
+    },
+    {
+      id: 2,
+      title: 'Outdoors',
+      imageUrl: 'https://i.ibb.co/kyW753T/outdoors.jpg'
+    },
+    {
+      id: 3,
+      title: 'Compact',
+      imageUrl: 'https://i.ibb.co/9G0yD9F/compact.jpg'
+    },
+    {
+      id: 4,
+      title: 'Overnight',
+      imageUrl: 'https://i.ibb.co/qs1N4vK/overnight.jpg'
+    },
+    {
+      id: 5,
+      title: 'Everyday',
+      imageUrl: 'https://i.ibb.co/qMXKvxQ/everyday.jpg'
+    }
+  ];
+
+
+  return (
+    <CategoryMenu categories={ categories }/>
+  );
+}
+
+export default Home;
