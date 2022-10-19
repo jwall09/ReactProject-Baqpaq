b3d692bc2ed10051f4e188745e6cab909ac085c3
Max Wallace <max.wallace9@yahoo.com>
2022-10-19 12:12:01 -0500

    starting the sign in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component.jsx'
 import Navigation from './routes/navigation/navigation.component.jsx'
+import SignIn from './routes/sign-in/sign-in.component.jsx';
+
+// This is the base for the Shop Page
+const Shop = () => {
+  return <h1>This is the shop page for now, we're working on it</h1>;
+};
 
 // This is the base for the home page of the website
 const App = () => {
@@ -11,6 +17,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element = {<Home />} />
+        <Route path ='shop' element= {<Shop />} />
+        <Route path ='sign-in' element= {<SignIn />} />
       </Route>
     </Routes>
   );
